ae3e1d1e7bf26b627320be155754488ef7f7baca
jhoffmcd <jhoffmcd@gmail.com>
2018-01-18 22:17:12 -0800

    Added robots.txt and icons.

--- preact.config.js
+++ preact.config.js
@@ -1,6 +1,15 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 export default (config, env, helpers) => {
   let { rule } = helpers.getLoadersByName(config, 'babel-loader')[0]
 
   // Add emotion css plugin and options at front of babel loader plugins.
   rule.options.plugins.unshift(['emotion', { 'hoist': env.production, 'sourceMap': !env.production, 'autoLabel': !env.production }])
+
+  // Add robots.txt to the workflow.
+  config.plugins.push(
+    new CopyWebpackPlugin([
+      { from: 'robots.txt' }
+    ])
+  )
 }
