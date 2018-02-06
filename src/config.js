129f21bb677bd6d9f55992575f87cae94a651f2c
jhoffmcd <jhoffmcd@gmail.com>
2018-02-05 22:42:39 -0800

    Added more config.

--- src/config.js
+++ src/config.js
@@ -2,80 +2,135 @@ export default {
   projects: [
     {
       title: 'StarCraft II',
+      client: 'Blizzard',
+      roles: ['Technical Lead', 'Front End'],
+      tech: ['html', 'css', 'js'],
       assets: {
+        main: 'sc2.jpg',
         thumb: 'sc2.jpg'
       },
-      href: 'projects/blizzard-starcraft-2'
+      href: 'projects/blizzard-starcraft-2',
+      external: 'https://starcraft2.com/en-us/'
     },
     {
       title: 'Overwatch World Cup',
+      client: 'Blizzard',
+      roles: ['Technical Lead', 'Front End'],
+      tech: ['html', 'css', 'js'],
       assets: {
+        main: 'owwc.jpg',
         thumb: 'owwc.jpg'
       },
-      href: 'projects/blizzard-overwatch-world-cup'
+      href: 'projects/blizzard-overwatch-world-cup',
+      external: 'https://worldcup.playoverwatch.com/en-us/'
     },
     {
       title: 'Heroes 2.0',
+      client: 'Blizzard',
+      roles: ['Technical Lead', 'Front End', 'Back End'],
+      tech: ['html', 'css', 'js', 'java'],
       assets: {
+        main: 'hots.jpg',
         thumb: 'hots.jpg'
       },
-      href: 'projects/blizzard-heroes-of-the-storm'
+      href: 'projects/blizzard-heroes-of-the-storm',
+      external: 'https://us.battle.net/heroes/en/'
     },
     {
       title: 'Heroes of the Dorm',
+      client: 'Blizzard',
+      roles: ['Front End'],
+      tech: ['html', 'css', 'js'],
       assets: {
+        main: 'hotd.jpg',
         thumb: 'hotd.jpg'
       },
-      href: 'projects/blizzard-heroes-of-the-dorm'
+      href: 'projects/blizzard-heroes-of-the-dorm',
+      external: 'https://heroesofthedorm.com/'
     },
     {
       title: 'Destiny 2 Comic Experience',
+      client: 'Bungie',
+      roles: ['Technical Lead', 'Front End'],
+      tech: ['html', 'css', 'js'],
       assets: {
+        main: 'd2comic.jpg',
         thumb: 'd2comic.jpg'
       },
-      href: 'projects/bungie-destiny-2-comic-reader'
+      href: 'projects/bungie-destiny-2-comic-reader',
+      external: 'https://comics.bungie.net/'
     },
     {
       title: 'Nexus Siege',
+      client: 'Riot Games',
+      roles: ['Front End'],
+      tech: ['html', 'css', 'js'],
       assets: {
+        main: 'lolns.jpg',
         thumb: 'lolns.jpg'
       },
-      href: 'projects/riot-games-nexus-siege'
+      href: 'projects/riot-games-nexus-siege',
+      external: 'https://na.leagueoflegends.com/en/featured/nexus-siege'
     },
     {
       title: 'Heroes Defy',
+      client: 'Blizzard',
+      roles: ['Front End'],
+      tech: ['html', 'css', 'js'],
       assets: {
+        main: 'defy.jpg',
         thumb: 'defy.jpg'
       },
-      href: 'projects/blizzard-heroes-defy'
+      href: 'projects/blizzard-heroes-defy',
+      external: 'https://heroesdefy.com/'
     },
     {
       title: 'Overwatch Comic Experience',
+      client: 'Blizzard',
+      roles: ['Front End'],
+      tech: ['html', 'css', 'js'],
       assets: {
+        main: 'owcomic.jpg',
         thumb: 'owcomic.jpg'
       },
-      href: 'projects/blizzard-overwatch-comic-reader'
+      href: 'projects/blizzard-overwatch-comic-reader',
+      external: 'https://comic.playoverwatch.com/'
     },
     {
       title: 'WoW Comic Experience',
+      client: 'Blizzard',
+      roles: ['Front End'],
+      tech: ['html', 'css', 'js'],
       assets: {
+        main: 'wowcomic.jpg',
         thumb: 'wowcomic.jpg'
       },
-      href: 'projects/blizzer-wow-comic-reader'
+      href: 'projects/blizzer-wow-comic-reader',
+      external: 'https://comic.worldofwarcraft.com/'
     },
     {
       title: 'War Dragons',
+      client: 'PocketGems',
+      roles: ['Front End'],
+      tech: ['html', 'css', 'js'],
       assets: {
+        main: 'wd.jpg',
         thumb: 'wd.jpg'
       },
-      href: 'projects/pocketgems-wardragons'
+      href: 'projects/pocketgems-wardragons',
+      external: 'http://wardragons.com/'
     },
     {
       title: 'Riot Games Engineering',
+      client: 'Riot Games',
+      roles: ['Front End', 'Back End'],
+      tech: ['html', 'css', 'js', 'php'],
       assets: {
+        main: 'rgen.jpg',
         thumb: 'rgen.jpg'
       },
-      href: 'projects/riot-games-engineering'
+      href: 'projects/riot-games-engineering',
+      external: 'https://engineering.riotgames.com/'
     }
   ]
 }
