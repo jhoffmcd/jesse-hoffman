cbf6996f539679ff5d399d599be5a401c816913e
Jesse Hoffman <jhoffmcd@gmail.com>
2020-04-23 15:52:59 -0700

    Add editor config and format 🛠✨

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,4 @@
-// ***********************************************************
+s; // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
 // You can change the location of this file or turn off loading
@@ -14,4 +14,4 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+};
