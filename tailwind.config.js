a2217a371c76df8cbc8cc937a6ef20291065e2b0
Jesse Hoffman <jhoffmcd@gmail.com>
2020-05-11 14:57:22 -0700

    Add basic theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    colors: {
+      purple: {
+        dark: "#1a184b",
+        medium: "#392758",
+        light: "#b64bcd",
+      },
+      magenta: "#da5ad2",
+      pink: "#dc94ba",
+      gold: "#f2d067",
+    },
   },
   variants: {},
   plugins: [],
