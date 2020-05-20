c451a6319d5f8588071b3b906be1c451dab0c587
Jesse Hoffman <jhoffmcd@gmail.com>
2020-05-20 11:20:17 -0700

    Remove old styles and Hex component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,31 +1,13 @@
 <script>
   import Nav from "../components/Nav.svelte";
-  import Hex from "../components/Hex.svelte";
   import Container from "../components/Container.svelte";
 
   export let segment;
 </script>
 
-<style>
-  /* header {
-    position: relative;
-  }
-
-  header::after {
-    content: "";
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    height: 1px;
-    @apply bg-retro;
-  } */
-</style>
-
 <header>
   <Container class="flex items-center justify-between">
     <div class="flex items-center py-2">
-      <!-- <Hex /> -->
       <div class="px-4">
         <a
           aria-current={segment === undefined ? 'page' : undefined}
