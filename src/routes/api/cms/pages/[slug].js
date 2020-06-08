638591291bd2e6d6f3e00d0c02c315ba7123deb8
Developer <developer@thes-mbp.lan>
2020-06-08 10:20:23 -0700

    Test homepage design layouts

---
+++ src/routes/api/cms/pages/[slug].js
@@ -0,0 +1,17 @@
+import initClient from "../_contentful/client";
+
+export async function get(req, res) {
+  const {
+    params: { slug },
+  } = req;
+
+  const client = initClient();
+
+  const entry = await client.getEntries({
+    "fields.slug": slug,
+    content_type: "page",
+  });
+
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(entry.items[0]));
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,67 @@
-<script>
-  import Container from "../components/Container.svelte";
+<script context="module">
+  export async function preload() {
+    try {
+      const res = await this.fetch(`api/cms/pages/about`);
+      const { fields } = await res.json();
+      return { pageHeading: fields.pageHeading, body: fields.body };
+    } catch (err) {
+      console.error(err);
+    }
+  }
 </script>
 
+<script>
+  import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+
+  import Container from "../components/Container.svelte";
+
+  export let pageHeading;
+  export let body;
+</script>
+
+<style>
+  .grid-visual {
+    display: grid;
+    grid-template-columns: repeat(12, [col-start] 1fr);
+  }
+
+  .grid-visual-1 {
+    grid-column: col-start / span 6;
+  }
+
+  .grid-visual-2 {
+    grid-column: col-start 7 / span 6;
+  }
+
+  .boxes {
+    width: 100%;
+    height: 100%;
+    background: lightblue;
+  }
+
+  .box-1 {
+    background: red;
+  }
+</style>
+
 <svelte:head>
-  <title>Jesse Hoffman</title>
+  <title>Jesse Hoffman | Engineering Manager | Technical Consultant</title>
 </svelte:head>
 
 <Container>
-  <p>About Me</p>
+  <div class="grid-visual">
+    <div class="grid-visual-1">
+      <h1>{pageHeading}</h1>
+    </div>
+    <div class="grid-visual-2">
+      <!-- {@html documentToHtmlString(body)} -->
+      <div class="boxes">
+        <div class="box-1" style="--aspect-ratio: 4/3"></div>
+        <div class="box-2"></div>
+        <div class="box-3"></div>
+        <div class="box-4"></div>
+      </div>
+    </div>
+    <!-- <div class="grid-visual-3"></div> -->
+  </div>
 </Container>
